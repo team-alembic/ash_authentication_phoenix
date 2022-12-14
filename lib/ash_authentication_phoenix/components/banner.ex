2d41030fc7e8ab87be1dcffe9c1675e2c2ec90dd
James Harton <james@harton.nz>
2022-12-15 07:38:53 +1300

    improvement(Components.Banner): Allow image, text and hrefs to be disabled with `nil`.

--- lib/ash_authentication_phoenix/components/banner.ex
+++ lib/ash_authentication_phoenix/components/banner.ex
@@ -2,11 +2,11 @@ defmodule AshAuthentication.Phoenix.Components.Banner do
   use AshAuthentication.Phoenix.Overrides.Overridable,
     root_class: "CSS class for the root `div` element.",
     href_class: "CSS class for the `a` tag.",
-    href_url: "A URL for the banner image to link to.",
+    href_url: "A URL for the banner image to link to. Set to `nil` to disable.",
     image_class: "CSS class for the `img` tag.",
-    image_url: "A URL for the `img` `src` attribute.",
+    image_url: "A URL for the `img` `src` attribute. Set to `nil` to disable.",
     text_class: "CSS class for the text `div`.",
-    text: "Banner text"
+    text: "Banner text. Set to `nil` to disable."
 
   @moduledoc """
   Renders a very simple banner at the top of the sign-in component.
@@ -26,6 +26,7 @@ defmodule AshAuthentication.Phoenix.Components.Banner do
     ~H"""
     <div class={override_for(@socket, :root_class)}>
       <%= case {override_for(@socket, :href_url), override_for(@socket, :image_url)} do %>
+        <% {nil, nil} -> %>
         <% {nil, img} -> %>
           <img class={override_for(@socket, :image_class)} src={img} />
         <% {hrf, img} -> %>
@@ -33,12 +34,18 @@ defmodule AshAuthentication.Phoenix.Components.Banner do
             <img class={override_for(@socket, :image_class)} src={img} />
           </a>
       <% end %>
-      <%= case  override_for(@socket, :text) do %>
-        <% text when is_binary(text) -> %>
+      <%= case  {override_for(@socket, :href_url), override_for(@socket, :text)} do %>
+        <% {nil, nil} -> %>
+        <% {nil, txt} -> %>
           <div class={override_for(@socket, :text_class)}>
-            <%= text %>
+            <%= txt %>
           </div>
-        <% _ -> %>
+        <% {hrf, txt} -> %>
+          <div class={override_for(@socket, :text_class)}>
+            <a class={override_for(@socket, :href_class)} href={hrf}>
+              <%= txt %>
+            </a>
+          </div>
       <% end %>
     </div>
     """
