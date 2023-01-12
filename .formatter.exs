3c6caf45ca1b51702f034fdca0d7cba3bcdeaa48
James Harton <james@harton.nz>
2023-01-13 11:24:17 +1300

    chore: fix formatter exports for macros.

--- .formatter.exs
+++ .formatter.exs
@@ -1,16 +1,18 @@
 # Used by "mix format"
 locals_without_parens = [
-  sign_in_route: 0,
   sign_in_route: 1,
   sign_in_route: 2,
   sign_in_route: 3,
   sign_out_route: 1,
   sign_out_route: 2,
   sign_out_route: 3,
-  auth_routes: 1,
-  auth_routes: 2,
-  auth_routes: 3,
-  set: 2
+  auth_routes_for: 1,
+  auth_routes_for: 2,
+  auth_routes_for: 3,
+  reset_route: 1,
+  set: 2,
+  ash_authentication_live_session: 1,
+  ash_authentication_live_session: 2 
 ]
 
 [
