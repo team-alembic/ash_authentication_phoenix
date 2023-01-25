1064cad7623a4a2a896a69d97f30d23f08e93018
Zach Daniel <zach@zachdaniel.dev>
2023-01-25 10:09:28 -0500

    docs: don't include readme.md on ash_hq

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule AshAuthentication.Phoenix.MixProject do
       ],
       docs: [
         main: "readme",
-        extras: ["README.md"],
+        extras: ["README.md": [
+          ash_hq: false
+        ]],
         formatters: ["html"],
         filter_modules: ~r/^Elixir.AshAuthentication.Phoenix/,
         source_url_pattern:
