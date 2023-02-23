8cceae10eaefe933c5209161f449a4cb2f5a9310
Zach Daniel <zach@zachdaniel.dev>
2023-02-23 00:40:15 -0500

    chore: fix typo

--- lib/ash_authentication_phoenix/router/on_live_view_mount.ex
+++ lib/ash_authentication_phoenix/router/on_live_view_mount.ex
@@ -5,8 +5,7 @@ defmodule AshAuthenticationPhoenix.Router.OnLiveViewMount do
   @spec on_mount(atom, %{required(String.t()) => any}, %{required(String.t()) => any}, Socket.t()) ::
           {:cont | :halt, Socket.t()}
   def on_mount(:default, _params, %{"otp_app" => otp_app}, socket) when not is_nil(otp_app) do
-    asign(socket, :otp_app, otp_app)
-    {:cont, socket}
+    {:cont, assign(socket, :otp_app, otp_app)}
   end
 
   def on_mount(_, _params, _session, socket), do: {:cont, socket}
