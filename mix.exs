c09b78395384acfa2e9b78226a3c1a53b9fce426
James Harton <james@harton.nz>
2024-01-24 10:56:35 +1300

    chore: Add `ex_check` and `sobelow`.

--- mix.exs
+++ mix.exs
@@ -123,13 +123,15 @@ defmodule AshAuthentication.Phoenix.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
       {:doctor, "~> 0.18", only: [:dev, :test]},
+      {:ex_check, "~> 0.15", only: [:dev, :test]},
       {:ex_doc, github: "elixir-lang/ex_doc", only: [:dev, :test], runtime: false},
       {:faker, "~> 0.17", only: [:dev, :test]},
       {:git_ops, "~> 2.4", only: [:dev, :test], runtime: false},
+      {:makeup_html, ">= 0.0.0", only: :dev, runtime: false},
       {:mimic, "~> 1.7", only: [:dev, :test]},
+      {:mix_audit, "~> 2.1", only: [:dev, :test]},
       {:plug_cowboy, "~> 2.5", only: [:dev, :test]},
-      # Syntax highlighting for HTML in ex_doc
-      {:makeup_html, ">= 0.0.0", only: :dev, runtime: false}
+      {:sobelow, "~> 0.13", only: [:dev, :test]}
     ]
   end
 
