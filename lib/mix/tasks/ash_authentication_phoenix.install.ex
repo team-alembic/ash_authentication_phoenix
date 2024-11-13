545999d65d53bb5716794480c78912df816ab38b
Rebecca Le <rebecca@alembic.com.au>
2024-11-13 12:11:34 +0800

    feat: Add action-specific flash messages to the generated AuthController

--- lib/mix/tasks/ash_authentication_phoenix.install.ex
+++ lib/mix/tasks/ash_authentication_phoenix.install.ex
@@ -252,14 +252,22 @@ defmodule Mix.Tasks.AshAuthenticationPhoenix.Install do
       use #{inspect(Igniter.Libs.Phoenix.web_module(igniter))}, :controller
       use AshAuthentication.Phoenix.Controller
 
-      def success(conn, _activity, user, _token) do
+      def success(conn, activity, user, _token) do
         return_to = get_session(conn, :return_to) || ~p"/"
+
+        message =
+          case activity do
+            {:confirm_new_user, :confirm} -> "Your email address has now been confirmed"
+            {:password, :reset} -> "Your password has successfully been reset"
+            _ -> "You are now signed in"
+          end
 
         conn
         |> delete_session(:return_to)
         |> store_in_session(user)
         # If your resource has a different name, update the assign name here (i.e :current_admin)
         |> assign(:current_user, user)
+        |> put_flash(:info, message)
         |> redirect(to: return_to)
       end
 
@@ -274,6 +282,7 @@ defmodule Mix.Tasks.AshAuthenticationPhoenix.Install do
 
         conn
         |> clear_session()
+        |> put_flash(:info, "You are now signed out")
         |> redirect(to: return_to)
       end
       """
