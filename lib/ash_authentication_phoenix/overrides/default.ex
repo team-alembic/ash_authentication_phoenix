01e1195149e0f9061cbdaa74c85d32679a0012d1
Steffen Beyer <steffen@beyer.io>
2025-01-17 11:43:43 +0100

    improvement: factor out shared input field CSS

--- lib/ash_authentication_phoenix/overrides/default.ex
+++ lib/ash_authentication_phoenix/overrides/default.ex
@@ -123,16 +123,18 @@ defmodule AshAuthentication.Phoenix.Overrides.Default do
     set :field_class, "mt-2 mb-2 dark:text-white"
     set :label_class, "block text-sm font-medium text-gray-700 mb-1 dark:text-white"
 
-    set :input_class, """
-    appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md
-    shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-pale-500
-    focus:border-blue-pale-500 sm:text-sm dark:text-black
-    """
-
-    set :input_class_with_error, """
-    appearance-none block w-full px-3 py-2 border border-red-400 rounded-md
+    @base_input_class """
+    appearance-none block w-full px-3 py-2 border rounded-md
     shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm
     dark:text-black
+    """
+
+    set :input_class, @base_input_class <> """
+    border-gray-300 focus:ring-blue-pale-500 focus:border-blue-pale-500
+    """
+
+    set :input_class_with_error, @base_input_class <> """
+    border-red-400
     """
 
     set :submit_class, """
