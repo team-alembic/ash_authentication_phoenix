202fe16590af859ab45dd96ef655879a038faf6e
Steffen Beyer <steffen@beyer.io>
2025-01-17 17:57:08 +0100

    chore: mix format

--- lib/ash_authentication_phoenix/overrides/default.ex
+++ lib/ash_authentication_phoenix/overrides/default.ex
@@ -129,13 +129,17 @@ defmodule AshAuthentication.Phoenix.Overrides.Default do
     dark:text-black
     """
 
-    set :input_class, @base_input_class <> """
-    border-gray-300 focus:ring-blue-pale-500 focus:border-blue-pale-500
-    """
+    set :input_class,
+        @base_input_class <>
+          """
+          border-gray-300 focus:ring-blue-pale-500 focus:border-blue-pale-500
+          """
 
-    set :input_class_with_error, @base_input_class <> """
-    border-red-400
-    """
+    set :input_class_with_error,
+        @base_input_class <>
+          """
+          border-red-400
+          """
 
     set :submit_class, """
     w-full flex justify-center py-2 px-4 border border-transparent rounded-md
