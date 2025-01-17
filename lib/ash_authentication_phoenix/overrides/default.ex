e2406137921eaadfe88ca31146459eb37c2ffc39
Steffen Beyer <steffen@beyer.io>
2025-01-17 11:27:37 +0100

    fix: duplicate border color CSS in defaults

--- lib/ash_authentication_phoenix/overrides/default.ex
+++ lib/ash_authentication_phoenix/overrides/default.ex
@@ -130,8 +130,8 @@ defmodule AshAuthentication.Phoenix.Overrides.Default do
     """
 
     set :input_class_with_error, """
-    appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md
-    shadow-sm placeholder-gray-400 focus:outline-none border-red-400 sm:text-sm
+    appearance-none block w-full px-3 py-2 border border-red-400 rounded-md
+    shadow-sm placeholder-gray-400 focus:outline-none sm:text-sm
     dark:text-black
     """
 
