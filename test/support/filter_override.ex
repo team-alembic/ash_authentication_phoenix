5acaaaf877d3bd76efb490d209d4ebbb72a6941b
kantum <42kantum@gmail.com>
2026-01-12 12:11:01 +0100

    fix: move filter override to support directory
    add SPDX license identifiers

--- test/support/filter_override.ex
+++ test/support/filter_override.ex
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Alembic Pty Ltd
+#
+# SPDX-License-Identifier: MIT
+
 defmodule AshAuthentication.Phoenix.Test.FilterInviteOverride do
   use AshAuthentication.Phoenix.Overrides
   alias AshAuthentication.Phoenix.Components
